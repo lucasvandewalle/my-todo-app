5f7c83d23044ce9ec2748b2fcfe3f7b2529862a2
lucas <lucas.vandewalle@gmail.com>
2023-11-19 20:08:20 +0100

    Change input box position

--- web.py
+++ web.py
@@ -14,6 +14,9 @@ st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase your productivity.")
 
+st.text_input(label="Enter a todo:", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
+
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
@@ -21,6 +24,3 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
-
-st.text_input(label="Enter a todo:", placeholder="Add new todo...",
-              on_change=add_todo, key="new_todo")
